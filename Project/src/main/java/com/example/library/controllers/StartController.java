70f649bfee58dc02799e9d8fbb6c71c75a2a902c
Iulian Popa <iulianp14@yahoo.com>
2022-04-28 21:59:42 +0300

    Library in CSharp

--- Project/src/main/java/com/example/library/controllers/StartController.java
+++
@@ -1,4 +0,0 @@
-package com.example.library.controllers;
-
-public class StartController {
-}
--- Project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.library {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.library to javafx.fxml;
-    exports com.example.library;
-    opens com.example.library.controllers to javafx.fxml;
-    exports com.example.library.controllers;
-}
